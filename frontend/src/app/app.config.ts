031a374d8cbfc4d2185608e1bc92cdb0403db3ce
Lam Nhan <lamnhan92@gmail.com>
2019-04-20 15:23:36 +0700

    init

---
+++ frontend/src/app/app.config.ts
@@ -0,0 +1,45 @@
+// app options
+export const APP_OPTIONS = {
+
+  // general information
+  siteName: 'Newsong',
+  email: 'contact@sheetbase.dev',
+  facebook: 'https://facebook.com/sheetbase',
+  twitter: 'https://twitter.com/sheetbase_dev',
+  github: 'https://github.com/sheetbase',
+  locale: 'en-US',
+
+  // currency config
+  currency: {
+    currencyCode: 'USD',
+    display: 'symbol',
+    digitsInfo: '1.0-2',
+  },
+
+  // date config
+  date: {
+    format: 'longDate',
+    timezone: '+0700',
+  },
+
+  // paypal client id
+  paypalClientId: 'sb',
+
+  // site default meta values
+  metas: {
+      title: 'Newsong',
+      description: 'The Newsong theme, based on Angular + Ionic.',
+      image: 'https://sheetbase.dev/assets/images/featured.jpg',
+      author: 'https://facebook.com/sheetbase', // optional
+
+      twitterCard: 'summary_large_image',
+      twitterSite: '@sheetbase_dev', // optional
+      twitterCreator: '@sheetbase_dev', // optional
+
+      ogType: 'website',
+      ogSiteName: 'Sheetbase Theme',
+      ogLocale: 'en_US', // optional
+      fbAppId: '276545556215548', // optional
+  },
+
+};
